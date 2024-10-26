d5a14d11824f14cc5493493b6b6db4d1f003f8eb
Archaick <saeed.jh3@gmail.com>
2024-10-26 10:36:00 +0700

    before adding CREATE method

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,25 @@ const App = () => {
 
   const usersCollectionRef = collection(db, "users");
 
-
   useEffect(() => {
     const getUsers = async () => {
-
       try {
         const data = await getDocs(usersCollectionRef);
-        setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+        // doc.data() doesn't normally add the id number. spread operator used with adjacent id
+        setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
-
     };
     getUsers();
-  }, []);
+  }, []); 
 
   return (
     <div>
       {users.map((user) => {
         return (
           <div>
-            <h1>Name: {user.name}</h1> <h1>Age: {user.age}</h1>
+            <h1>Name: {user.name}</h1> <h1>Age: {user.age}</h1> <h1>ID: {user.id}</h1>
           </div>
         );
       })}
